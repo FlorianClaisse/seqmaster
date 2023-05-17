8d4b8b6dbf4c929a9351b020da699b538478147b
FlorianClaisse <floc.claisse@gmail.com>
2023-05-17 10:19:40 +0200

    Add exemple for all program

--- ContigDiff/contig_diff.cpp
+++ ContigDiff/contig_diff.cpp
@@ -12,14 +12,22 @@
 #include "../Foundation/include/directory.h"
 #include "include/contig_diff.h"
 
-// TODO: Optimisation (taille en generale des chaine commune (i++ ou i--))
+// TODO: Optimisation
+// TODO: Amelioration supprimer les chaines doubles
 
 using namespace std;
 namespace fs = std::filesystem;
 
 namespace contig_diff {
-    void search_with_2_file(const fs::path &first_path, const fs::path &second_path, map<string, string> &common);
+    typedef struct {
+        string filename;
+        string inputB;
+        int percentage;
+    } Common;
+
+    void search_with_2_file(const fs::path &first_path, const fs::path &second_path, map <string, Common> &common);
     unsigned long find_inside_file(ifstream &test_file, const string &word);
+    void check_on_directory(const fs::path &directory_path, map <string, Common> &common);
 }
 
 int contig_diff::main(const contig_diff::option &options) {
@@ -35,13 +43,12 @@ int contig_diff::main(const contig_diff::option &options) {
         return EXIT_FAILURE;
     }
 
-    // [contig_value, (contig_name -> filename)]
-    map<string, string> common;
+    // [sub_contig_value, Common]
+    map<string, Common> common;
 
     // Init common with 2 file
     pair<fs::path, fs::path> two_first = directory::two_first_fasta(options.inputA);
     search_with_2_file(two_first.first, two_first.second, common);
-    cout << common.size() << endl;
 
     // Compare first file with all files
     for (const auto &path: fs::directory_iterator(options.inputA)) {
@@ -51,47 +58,13 @@ int contig_diff::main(const contig_diff::option &options) {
     }
 
     // Check all possible common inside all file in A.
-    unsigned long max_size;
-    ifstream *current_file;
-    string sub;
-    vector<string> value_to_remove;
-    map<string, string> value_to_add;
-    for (const auto &path: fs::directory_iterator(options.inputA)) {
-        if (!directory::is_fastaline_file(path)) continue;
-        current_file = directory::read_open(path);
-        // Check all common in one file
-        for (const auto &value: common) {
-            max_size = find_inside_file((*current_file), value.first);
-            if (max_size != value.first.size()) {
-                sub = value.first.substr(0, max_size);
-                if (common.find(sub) == common.end() && value_to_add.find(sub) == value_to_add.end()) {
-                    value_to_add[sub] = value.second;
-                    value_to_remove.push_back(value.first);
-                    cout << "Max size : " << max_size << endl;
-                    cout << "Value to remove : " << value.first << endl;
-                    cout << "Value to add : " << sub << endl;
-                }
-            }
-        }
-        directory::read_close(current_file);
-        cout << "Remove : " << value_to_remove.size() << endl;
-        cout << "Add : " << value_to_add.size() << endl;
+    check_on_directory(options.inputA, common);
 
-        for (const auto &value: value_to_add) {
-            common[value.first] = value.second;
-        }
-        value_to_add.clear();
-
-        for (const auto &value: value_to_remove) {
-            common.erase(value);
-        }
-        value_to_remove.clear();
-    }
 
     return EXIT_SUCCESS;
 }
 
-void contig_diff::search_with_2_file(const fs::path &first_path, const fs::path &second_path, map<string, string> &common) {
+void contig_diff::search_with_2_file(const fs::path &first_path, const fs::path &second_path, map<string, Common> &common) {
     string filename = first_path.stem();
     ifstream *first_file = directory::read_open(first_path);
     ifstream *test_file = directory::read_open(second_path);
@@ -100,12 +73,14 @@ void contig_diff::search_with_2_file(const fs::path &first_path, const fs::path 
     while(getline((*first_file), first_line_read)) {
         if (first_line_read.at(0) == '>') {
             contig_name = first_line_read.append(" -> " + filename);
+            contig_name.erase(remove(contig_name.begin(), contig_name.end(), '\n'), contig_name.end());
+            contig_name.erase(remove(contig_name.begin(), contig_name.end(), '\r'), contig_name.end());
         } else {
             unsigned long size = find_inside_file((*test_file), first_line_read);
             if (size != 0) {
                 string sub = first_line_read.substr(0, size);
                  if (common.find(sub) == common.end()) {
-                     common[sub] = contig_name;
+                     common[sub] = {contig_name, "", 100};
                  }
             }
             if (test_file->eof()) test_file->clear();
@@ -133,5 +108,45 @@ unsigned long contig_diff::find_inside_file(ifstream &test_file, const string &w
 
     return max_size;
 }
+
+void contig_diff::check_on_directory(const fs::path &directory_path, map<string, Common> &common) {
+    unsigned long max_size;
+    ifstream *current_file;
+    string sub;
+    vector<string> value_to_remove;
+    map<string, Common> value_to_add;
+    for (const auto &path: fs::directory_iterator(directory_path)) {
+        if (!directory::is_fastaline_file(path)) continue;
+        current_file = directory::read_open(path);
+        // Check all common in one file
+        for (const auto &value: common) {
+            max_size = find_inside_file((*current_file), value.first);
+            if (max_size != value.first.size()) {
+                sub = value.first.substr(0, max_size);
+                if ( max_size != 0) {
+                    value_to_add[value.first] = {sub, "", 100};
+                }
+                value_to_remove.push_back(value.first);
+                cout << "Max size : " << max_size << ", Value to remove : " << value.first << ", Value to add : " << sub << endl;
+            }
+
+            if (current_file->eof()) current_file->clear();
+            current_file->seekg(0, ios::beg);
+        }
+        directory::read_close(current_file);
+        cout << "Remove : " << value_to_remove.size() << ", Add : " << value_to_add.size() << endl;
+
+        for (const auto &value: value_to_remove) {
+            common.erase(value);
+        }
+        value_to_remove.clear();
+
+        for (const auto &value: value_to_add) {
+            common[value.first] = value.second;
+        }
+        value_to_add.clear();
+    }
+}
+
 
 
